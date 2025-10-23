ef01e13e564fb22808f5d05771883968814654f2
MalyginOleg <Omalygin@korusconsulting.ru>
2025-10-23 15:13:14 +0300

    getUserAuthData

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,5 @@
-export { getUserAuthData } from "./model/selectors/getUserAuthData/getUserAuthData";
 export { userSlice, userActions } from "./model/slice/userSlice";
 export { UserSchema, User } from "./model/types/user";
+
+export { getUserAuthData } from "./model/selectors/getUserAuthData/getUserAuthData";
+export { getUserInited } from "./model/selectors/getUserInited/getUserInited";
